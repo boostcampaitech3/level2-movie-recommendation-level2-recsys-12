e3ad29d6232e42e986b885232cb2cf8baa4a897f
barobaro9842 <baro9842@gmail.com>
2022-04-12 15:58:33 +0900

    Graph rec complete training, todo :  Inference

--- seunghwan/GraphRec/dataset.py
+++ seunghwan/GraphRec/dataset.py
@@ -1,8 +1,10 @@
+import enum
 import os
 from tqdm import tqdm
 import time 
 import pickle
 from copy import deepcopy
+from operator import itemgetter
 
 import numpy as np
 import pandas as pd
@@ -23,6 +25,7 @@ class GraphRecDataset() :
         
         # encoding_data
         self.encoded_data, self.user_encoder, self.movie_encoder = self.__label_encode(self._raw_data)
+        # self.negative_data = self.__get_negative(self.encoded_data)
         
         # unique users and movies
         self.origin_users = np.unique(self.origin_data['user'])
@@ -39,16 +42,26 @@ class GraphRecDataset() :
         self.inference_data = deepcopy(self.encoded_data)
         
         # to graph
-        self.train_user_graph, self.train_movie_graph = self.__graph_initailize('train')
-        self.inference_user_graph, self.inference_movie_graph = self.__graph_initailize('inference')
+        self.train_user_graph, self.train_movie_graph, self.train_user_adj, self.train_movie_adj = self.__graph_initailize('train')
+        self.test_user_graph, self.test_movie_graph, self.test_user_adj, self.test_movie_adj = self.__graph_initailize('test')
+        self.inference_user_graph, self.inference_movie_graph, self.inference_user_adj, self.inference_movie_adj = self.__graph_initailize('inference')
+        
         self.n_features_user = self.train_user_graph.shape[1]   # inference와 같음
         self.n_features_movie = self.train_movie_graph.shape[1] # inference와 같음
+
+        # get negative
+        self.train_user_negative = self.__get_negative(self.train_user_adj)
+        self.train_movie_negative = self.__get_negative(self.train_movie_adj)
+        self.test_user_negative = self.__get_negative(self.test_user_adj)
+        self.test_movie_negative = self.__get_negative(self.test_movie_adj)
+        self.inference_user_negative = self.__get_negative(self.inference_user_adj)
+        self.inference_movie_negative = self.__get_negative(self.inference_movie_adj)
         
     def __drop_time_add_rating(self, data) :
         data.drop('time', axis=1, inplace=True)
         data['rating'] = 1.0
         return data
-    
+
     def __label_encode(self, data):
         user_encoder = LabelEncoder()
         movie_encoder = LabelEncoder()
@@ -73,15 +86,26 @@ class GraphRecDataset() :
     def __graph_initailize(self, data_type) :
         
         if data_type == 'train' : data = self.train_data 
+        if data_type == 'test' : data = self.test_data
         if data_type == 'inference' : data = self.inference_data
         
+        adj_u_ck = os.path.exists(os.path.join(self._dirs.output, f'{data_type}_adj_users.pkl'))
+        adj_m_ck = os.path.exists(os.path.join(self._dirs.output, f'{data_type}_adj_movies.pkl'))
+        dgr_u_ck = os.path.exists(os.path.join(self._dirs.output, f'{data_type}_dgr_users.pkl'))
+        dgr_m_ck = os.path.exists(os.path.join(self._dirs.output, f'{data_type}_dgr_movies.pkl'))
+        
         # load existing files if they exist        
-        if self._args.use_exist and os.path.exists(os.path.join(self._dirs.output, f'{data_type}_user_graph.pkl')) and os.path.exists(os.path.join(self._dirs.output, f'{data_type}_movie_graph.pkl')):
+        if self._args.use_exist and adj_u_ck and adj_m_ck and dgr_u_ck and dgr_m_ck :
+            
             print(f'load existing {data_type} graphs!')
-            with open(os.path.join(self._dirs.output, 'train_user_graph.pkl'), 'rb') as f :
-                user_graph = pickle.load(f)
-            with open(os.path.join(self._dirs.output, 'train_movie_graph.pkl'), 'rb') as f :
-                movie_graph = pickle.load(f)  
+            with open(os.path.join(self._dirs.output, f'{data_type}_adj_users.pkl'), 'rb') as f :
+                adj_users = pickle.load(f)
+            with open(os.path.join(self._dirs.output, f'{data_type}_adj_movies.pkl'), 'rb') as f :
+                adj_movies = pickle.load(f)
+            with open(os.path.join(self._dirs.output, f'{data_type}_dgr_users.pkl'), 'rb') as f :
+                dgr_users = pickle.load(f)
+            with open(os.path.join(self._dirs.output, f'{data_type}_dgr_movies.pkl'), 'rb') as f :
+                dgr_movies = pickle.load(f)
         
         # else initialize
         else :
@@ -101,25 +125,30 @@ class GraphRecDataset() :
                 dgr_users[user_id][0] += 1   #해당 유저의 degree 증가
                 dgr_movies[movie_id][0] += 1 #해당 영화의 degree 증가
                 
-            max_dgr_user = np.max(dgr_users)
-            max_dgr_movie = np.max(dgr_movies)
-            std_dgr_users = np.true_divide(dgr_users, max_dgr_user)   # 정규화
-            std_dgr_movies = np.true_divide(dgr_movies, max_dgr_movie)# 정규화 
-            
-            # 유저 수 Identity 행렬(31360) + 유저 그래프 행렬(6807) + 유저 그래프 차수(1) =  31360 * 38168
-            user_graph = np.concatenate((np.identity(self.user_num, dtype=bool), adj_users, std_dgr_users), axis=1)      
-            movie_graph = np.concatenate((np.identity(self.movie_num, dtype=bool), adj_movies, std_dgr_movies), axis=1)
-            
-            with open(os.path.join(self._dirs.output, f'{data_type}_user_graph.pkl'), 'wb') as f :
-                pickle.dump(user_graph, f)
-            with open(os.path.join(self._dirs.output, f'{data_type}_movie_graph.pkl'), 'wb') as f :
-                pickle.dump(movie_graph, f)
+            with open(os.path.join(self._dirs.output, f'{data_type}_adj_users.pkl'), 'wb') as f :
+                pickle.dump(adj_users, f)
+            with open(os.path.join(self._dirs.output, f'{data_type}_adj_movies.pkl'), 'wb') as f :
+                pickle.dump(adj_movies, f)
+            with open(os.path.join(self._dirs.output, f'{data_type}_dgr_users.pkl'), 'wb') as f :
+                pickle.dump(dgr_users, f)
+            with open(os.path.join(self._dirs.output, f'{data_type}_dgr_movies.pkl'), 'wb') as f :
+                pickle.dump(dgr_movies, f)
+        
+        max_dgr_user = np.max(dgr_users)
+        max_dgr_movie = np.max(dgr_movies)
+        std_dgr_users = np.true_divide(dgr_users, max_dgr_user)   # 정규화
+        std_dgr_movies = np.true_divide(dgr_movies, max_dgr_movie)# 정규화 
+        
+        # 유저 수 Identity 행렬(31360) + 유저 그래프 행렬(6807) + 유저 그래프 차수(1) =  31360 * 38168
+        user_graph = np.concatenate((np.identity(self.user_num, dtype=bool), adj_users, std_dgr_users), axis=1)      
+        movie_graph = np.concatenate((np.identity(self.movie_num, dtype=bool), adj_movies, std_dgr_movies), axis=1)
+    
         
         if self._args.use_side_information is True :
             print(f'load {data_type} side information...')
             movie_garph = self.__concat_side_information(data=movie_graph, side='movie')
         
-        return user_graph, movie_garph
+        return user_graph, movie_garph, adj_users, adj_movies
                 
     def __concat_side_information(self, data, side):
         if side == 'movie' :
@@ -151,4 +180,25 @@ class GraphRecDataset() :
                 
             return np.concatenate((data, years, genres), axis=1)
 
-            
+    def __get_negative(self, data) :
+        
+        neg_dict = dict()
+        
+        for id, samples in enumerate(data) :
+            neg_dict[id] = np.where(samples == 0)[0]
+        
+        return neg_dict
+        # dir_negative = os.path.join(self._dirs.output, 'negative.csv')
+        # if os.path.exists(dir_negative):
+        #     data = pd.read_csv(dir_negative)
+        # else :
+        #     print('negative_sampling...')
+        #     table = data.pivot_table('rating', index='item', columns='user')
+        #     table.fillna(0.0, inplace=True)
+        #     data = pd.DataFrame(table.unstack())
+        #     data.reset_index(inplace=True)
+        #     data = data.rename(columns={0:'rating'})
+        #     data.drop(data[data['rating']==1.0].index, inplace=True)
+        
+        # return data
+        
--- seunghwan/GraphRec/model.py
+++ seunghwan/GraphRec/model.py
@@ -9,6 +9,7 @@ class GraphRecModel(nn.Module) :
         
         self.user_weight = args.user_weight
         self.movie_weight = args.movie_weight
+
         self.user_input_size = dataset.n_features_user
         self.movie_input_size = dataset.n_features_movie
         self.mf_size = args.mf_size
@@ -16,18 +17,20 @@ class GraphRecModel(nn.Module) :
         # layer
         self.user_layer = nn.Linear(self.user_input_size, self.mf_size)
         self.movie_layer = nn.Linear(self.movie_input_size, self.mf_size)
-        
+        self.sigmoid_layer = nn.Sigmoid()
         self.__initialize_weights()
                 
     def forward(self, user_input, movie_input) :
         
         user_mf = self.user_layer(user_input)
-        user_mf = np.concatenate(F.relu(user_mf), F.relu(-user_mf), axis=1)
+        user_mf = torch.cat((F.relu(user_mf), F.relu(-user_mf)), axis=1)
         movie_mf = self.movie_layer(movie_input)
-        movie_mf = np.concatenate(F.relu(movie_mf), F.relu(-movie_mf), axis=1)
+        movie_mf = torch.cat((F.relu(movie_mf), F.relu(-movie_mf)), axis=1)
         
-        input = torch.multiply(user_mf, movie_mf)
-        pred = torch.sum(input, dim=1)
+        output = torch.multiply(user_mf, movie_mf)
+        pred = torch.sum(output, dim=1)
+        
+        # pred = self.sigmoid_layer(pred)
         
         reg_user = self.user_weight * (torch.sum(user_mf**2) / 2)
         reg_movie = self.movie_weight * (torch.sum(movie_mf**2) / 2)
